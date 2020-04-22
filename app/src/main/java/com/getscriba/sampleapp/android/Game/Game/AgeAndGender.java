68ed41c8ca89855cb70bb797cebb4e498434aed1
x16438934 <eo4short@hotmail.com>
2020-04-22 16:12:37 +0100

    AddAgeAndGender activity added

--- app/src/main/java/com/getscriba/sampleapp/android/Game/Game/AgeAndGender.java
+++ app/src/main/java/com/getscriba/sampleapp/android/Game/Game/AgeAndGender.java
@@ -1,10 +1,7 @@
 package com.getscriba.sampleapp.android.Game.Game;
 
-
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
@@ -19,50 +16,33 @@ public class AgeAndGender extends AppCompatActivity {
 //    tutorial that I did to make a popup https://www.youtube.com/watch?v=fn5OlqQuOCk&ab_channel=FilipVujovic
 
     private String userGender;
-    private String userAge;
+    private int userAge;
+    private SharedPreferences settings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_age_and_gender);
-
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-
-        int phoneWidth = dm.widthPixels;
-        int phoneHeight = dm.heightPixels;
-
-        SharedPreferences settings = this.getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
-        String Age = null;
-        try {
-            Age = settings.getString("Age", "3");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        userGender = settings.getString("Gender", "Female");
-
-
-        getWindow().setLayout(phoneWidth,(int)(phoneHeight*.5));
-
+        settings = getSharedPreferences("UserInfo", 0);
+        userAge = 0;
+        userGender = "Female";
 
 //        used this tutorial to get a number picker https://www.zoftino.com/android-number-picker-tutorial.
         NumberPicker mNumberPicker = findViewById(R.id.numberPicker);
-        if (Age != null) {
-            mNumberPicker.setValue(Integer.parseInt(Age));
-        }
-        mNumberPicker.setMinValue(3);
+        mNumberPicker.setValue(userAge);
+        mNumberPicker.setMinValue(0);
         mNumberPicker.setMaxValue(99);
         mNumberPicker.setOnValueChangedListener(onValueChangeListener);
 
         Button btnChangeAgeGender = findViewById(R.id.save_btn);
-
         btnChangeAgeGender.setOnClickListener(v -> saveGenderAge());
     }
+
+
     private NumberPicker.OnValueChangeListener onValueChangeListener =
-            (numberPicker, i, i1) -> {
-                userAge = String.valueOf(numberPicker.getValue());
-            };
+            (numberPicker, i, i1) -> userAge = numberPicker.getValue();
 
     public void onGenderButton(View view) {
         // Is the button now checked?
@@ -85,10 +65,9 @@ public class AgeAndGender extends AppCompatActivity {
     private void saveGenderAge() {
 //      used code from this website to save the Age and Gender of the user
 //      https://stackoverflow.com/questions/10209814/saving-user-information-in-app-settings
-        SharedPreferences settings = getSharedPreferences("UserInfo", 0);
         SharedPreferences.Editor editor = settings.edit();
-        editor.putString("Age", userAge);
-        editor.putString("Gender", userGender);
+        editor.putInt("userAge", userAge);
+        editor.putString("userGender", userGender);
         editor.apply();
         Toast.makeText(this, "The users Age was Saved as: "+userAge+" and Gender was Saved As: "+ userGender, Toast.LENGTH_SHORT).show();
         this.finish();
